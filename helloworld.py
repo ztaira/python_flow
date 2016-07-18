5f1eb711891959663a7a6279541255bc1d840eec
Zachary Taira <zach.taira@gmail.com>
2016-07-17 21:45:31 -0400

    Completion Update 7/17
    It works now! :D Included updated cpp file, example python file, and
    image.

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,35 @@
 def main():
     main_1()
+    x = 1;
+    x = 1;
     main_2()
+    x = 1;
+    x = 1;
     main_3()
+    x = 1;
 
 def main_1():
+    x = 1;
+    x = 1;
+    x = 1;
+    x = 1;
     main_2()
 
 def main_2():
+    x = 1;
+    x = 1;
     main_3()
+    x = 1;
+    x = 1;
 
 def main_3():
+    x = 1;
+    x = 1;
+    x = 1;
 
 if __name__ == '__main__':
+    x = 1;
+    x = 1;
     main()
-
+    x = 1;
+    x = 1;
--- python_flow.cpp
+++ python_flow.cpp
@@ -7,6 +7,8 @@
 #include <string>
 // regex
 #include <regex>
+// stack
+#include <stack>
 // setw
 #include <iomanip>
 using namespace std;
@@ -15,8 +17,11 @@ using namespace std;
 // q: but what if the file's too big?
 // a: i'll deal with that if I have time
 vector<string> line_array;
+stack<int> jump_back;
 
-int scan_function(string func_name, int level);
+int scan_function(string func_name, int level, int lastpos);
+int follow_code(int start, int end);
+int jump_code(int start, int end);
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +29,7 @@ int main(int argc, char *argv[])
     int start;
     int end;
     int level=0;
+    int last_position;
 
     // read in file to vector of strings
     ifstream workfile (argv[1]);
@@ -32,6 +38,7 @@ int main(int argc, char *argv[])
     {   
         line_array.push_back(line);
     }
+    workfile.close();
 
     // equalize lengths of vector strings
     // if string is less than 80, add spaces
@@ -39,7 +46,7 @@ int main(int argc, char *argv[])
     for (vector<string>::iterator itr = line_array.begin();
          itr != line_array.end(); ++itr)
     {
-        while (itr->length() < 80)
+        while (itr->length() < 40)
         {
             itr->push_back(' ');
         }
@@ -67,10 +74,10 @@ int main(int argc, char *argv[])
         if (regex_search(line_array[i], match_results, main_func))
         {
             start = i;
-            cout << i << endl;
+            cout << i+1 << endl;
         }
     }
-    cout << "Scan start: " << start << endl << "Scan end: " << end << endl;
+    cout << "Scan start: " << start+1 << endl << "Scan end: " << end << endl;
 
     // scan from start to end for a function call
     // types to account for:
@@ -78,23 +85,32 @@ int main(int argc, char *argv[])
     // object declaration, e.g. x = object()
     // object method, e.g. x = object.obj_func()
     // if one is found, go scan that one too
+    last_position = start;
     cout << "Starting scan:" << endl << endl;
-    cout << level << "if __name__ == .__main__.:" << endl;
+    // cout << level << "if __name__ == .__main__.:" << endl;
     regex func ("(\\S+)\\(.*\\)");
     for (int i=start; i<end; ++i)
     {
         if (regex_search(line_array[i], match_results, func))
         {
-            cout << right << setw(10) << level+1 << match_results[1] << endl;
-            scan_function(match_results[1], level+1);
+            follow_code(last_position, i);
+            last_position=i;
+            // cout << level+1 << match_results[1] << endl;
+            jump_back.push(i);
+            scan_function(match_results[1], level+1, i);
         }
     }
 
-    workfile.close();
+    follow_code(last_position, end-1);
+
+    for (int i=0; i<line_array.size(); ++i)
+    {
+        cout << left << setw(4) << i+1 << line_array[i] << endl;
+    }
     return 1;
 }
 
-int scan_function(string func_name, int level)
+int scan_function(string func_name, int level, int lastpos)
 {
     // set variables to hold start, end
     int start = -1;
@@ -110,6 +126,8 @@ int scan_function(string func_name, int level)
     {
         if (regex_search(line_array[i], match_results, declaration))
         {
+            jump_code(lastpos, i);
+            lastpos = i;
             start = i;
         }
     }
@@ -118,8 +136,9 @@ int scan_function(string func_name, int level)
         return 0;
     }
     
+    
     // regex to match end
-    regex ender ('^'+match_results[1].str()+"\\S");
+    regex ender ("^\\S");
     // set the end variable to when the indent is less
     for (int i=start+1; i<line_array.size(); ++i)
     {
@@ -137,10 +156,102 @@ int scan_function(string func_name, int level)
     {
         if (regex_search(line_array[i], match_results, func))
         {
-            cout << right << setw(10) << level+1 << match_results[1] << endl;
-            scan_function(match_results[1], level+1);
+            follow_code(lastpos, i);
+            lastpos = i;
+            // cout << level+1 << match_results[1] << endl;
+            jump_back.push(i);
+            scan_function(match_results[1], level+1, i);
         }
     }
+
+    follow_code(lastpos, end-1);
+    jump_code(end-1, jump_back.top());
+    jump_back.pop();
     
+    return end;
+}
+
+// * 
+// |
+// |
+// *
+int follow_code(int start, int end)
+{
+    for (int i=0; i<line_array.size(); ++i)
+    {
+        if (i > start and i < end)
+        {
+            line_array[i].push_back('|');
+        }
+        else if (i == start or i == end)
+        {
+            line_array[i].push_back('*');
+        }
+        else
+        {
+            line_array[i].push_back(' ');
+        }
+    }
     return 1;
 }
+
+// .v  >.
+//  :  :
+//  >..^
+int jump_code(int start, int end)
+{
+    // set direction
+    char direction = ' ';
+    int switchvar;
+    if (start < end)
+    {
+        direction = 'v';
+    }
+    else
+    {
+        direction = '^';
+    }
+    
+    // draw start and end lines
+    line_array[start].push_back('.');
+    line_array[start].push_back(direction);
+    line_array[start].push_back(' ');
+
+    line_array[end].push_back(' ');
+    line_array[end].push_back('>');
+    line_array[end].push_back('.');
+
+    if (end < start)
+    {
+        switchvar = start;
+        start = end;
+        end = switchvar;
+    }
+
+    // draw everything else
+    for (int i=0; i<line_array.size(); ++i)
+    {
+        if (i != start and i != end)
+        {
+            if (i > start and i < end)
+            {
+                line_array[i].push_back(' ');
+                line_array[i].push_back(':');
+                line_array[i].push_back(' ');
+            }
+            else
+            {
+                line_array[i].push_back(' ');
+                line_array[i].push_back(' ');
+                line_array[i].push_back(' ');
+            }
+        }
+    }
+
+    return 1;
+}
+
+
+
+
+
